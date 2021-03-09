912083ce2d152826284f4ff8a1b0cdd994dc0cf3
avi9a <73037370+avi9a@users.noreply.github.com>
2021-03-09 21:01:35 +0300

    feat: add variables

--- 3D Platformer/Assets/Scripts/PlayerController.cs
+++ 3D Platformer/Assets/Scripts/PlayerController.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
+    public float speed = 5.0f;
+    public float maxSpeed = 8.0f;
+    public float jumpForce = 6.0f;
+    public Transform groundCheck;
     void Start() {
     }
     void Update() {
