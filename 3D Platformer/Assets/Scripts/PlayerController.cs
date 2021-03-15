b26e4f109bf05121468b87369ddb9f8202876459
avi9a <73037370+avi9a@users.noreply.github.com>
2021-03-15 18:47:23 +0300

    fix: player movement

--- 3D Platformer/Assets/Scripts/PlayerController.cs
+++ 3D Platformer/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour {
     public float turnSpeed = 3.0f;
     public float maxSpeed = 8.0f;
     public float jumpForce = 6.0f;
+    private Vector3 forwardMovement;
     public Transform groundCheck;
     private CharacterController characterController;
     void Awake() {
@@ -21,10 +22,9 @@ public class PlayerController : MonoBehaviour {
     void Movement() {
         float verticalInput = Input.GetAxis("Vertical");
         float horizontalInput = Input.GetAxis("Horizontal");
-        float curSpeed = speed * verticalInput;
-        Vector3 forwardMovement = transform.TransformDirection(Vector3.forward);
-        characterController.SimpleMove(forwardMovement * curSpeed);
+        forwardMovement = new Vector3(0.0f, 0.0f, verticalInput);
+        forwardMovement = transform.TransformDirection(forwardMovement);
+        characterController.Move(forwardMovement * speed * Time.deltaTime);
         transform.Rotate(Vector3.up * turnSpeed * horizontalInput);
-        
     }
 }
