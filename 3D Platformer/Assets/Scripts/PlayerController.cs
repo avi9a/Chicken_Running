06b00ca85006898b5be2f2ef7359dc1a8ddffb94
avi9a <73037370+avi9a@users.noreply.github.com>
2021-03-15 19:04:07 +0300

    fix: player movement

--- 3D Platformer/Assets/Scripts/PlayerController.cs
+++ 3D Platformer/Assets/Scripts/PlayerController.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
-    public float speed = 5.0f;
+    public float speed;
+    public float walkSpeed = 4.0f;
+    public float runSpeed = 6.0f;
     public float turnSpeed = 3.0f;
     public float maxSpeed = 8.0f;
     public float jumpForce = 6.0f;
+    private float gravity = 10.0f;
     private Vector3 forwardMovement;
     public Transform groundCheck;
     private CharacterController characterController;
@@ -23,8 +26,25 @@ public class PlayerController : MonoBehaviour {
         float verticalInput = Input.GetAxis("Vertical");
         float horizontalInput = Input.GetAxis("Horizontal");
         forwardMovement = new Vector3(0.0f, 0.0f, verticalInput);
-        forwardMovement = transform.TransformDirection(forwardMovement);
+        if(forwardMovement != Vector3.zero && Input.GetButton("Up")) {
+            Walk();
+        } else if(forwardMovement != Vector3.zero && Input.GetButton("Up")) {
+            Run();
+        } else if(forwardMovement == Vector3.zero) {
+            Idle();
+        }
+        forwardMovement *= speed;
         characterController.Move(forwardMovement * speed * Time.deltaTime);
         transform.Rotate(Vector3.up * turnSpeed * horizontalInput);
     }
+    public void Idle() {
+
+    }
+    public void Walk() {
+        speed = walkSpeed;
+    }
+    public void Run() {
+        speed = runSpeed;
+    }
+
 }
